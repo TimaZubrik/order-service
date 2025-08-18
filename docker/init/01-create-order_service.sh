5ae6409e35d079677c5e58462ea6688de3e41710
Timofey Zubrik <timazubrik@gmail.com>
2025-08-18 12:01:08 +0300

    changed db init file + docker-compose.yaml

--- docker/init/01-create-order_service.sh
+++ docker/init/01-create-order_service.sh
@@ -1,17 +1,17 @@
+#!/usr/bin/env bash
 set -e
 
-if [ -z "$ORDER_DB_NAME" ] || [ -z "$DB_USER" ]; then
-  echo "ERROR: ORDER_DB_NAME and DB_USER must be set"
-  exit 1
-fi
 
-exists=$(psql -U "$DB_USER" -tAc \
-  "SELECT 1 FROM pg_database WHERE datname = '$ORDER_DB_NAME'")
+DB="$POSTGRES_DB"
+USER="$POSTGRES_USER"
+
+
+exists=$(psql -U "$USER" -tAc \
+  "SELECT 1 FROM pg_database WHERE datname = '$DB'")
 
 if [ "$exists" != "1" ]; then
-  echo "Creating database \"$ORDER_DB_NAME\" owned by \"$DB_USER\""
-  psql -U "$DB_USER" -c \
-    "CREATE DATABASE \"$ORDER_DB_NAME\" OWNER \"$DB_USER\""
+  echo "Создаём базу $DB владельцем $USER"
+  psql -U "$USER" -c "CREATE DATABASE \"$DB\" OWNER \"$USER\""
 else
-  echo "Database \"$ORDER_DB_NAME\" already exists, skipping"
+  echo "База $DB уже существует"
 fi
